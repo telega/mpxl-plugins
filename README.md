95cd678a07363502701f435bc8031e0702485f1c
Thomas Allen <tom@telega.org>
2018-12-30 13:00:05 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-# Webpack-TypeScript-Babel
+# mpxl-plugins
 
-> This is sample repository demonstrates how to use Webpack, TypeScript and Babel
-
-## Building the repo
-
-```shell
-npm run build
-```
-
-## Building only types
-
-```shell
-npm run build:types
-```
-
-## Type-Checking the repo
-
-```shell
-npm run type-check
-```
-
-And to run in --watch mode:
-
-```shell
-npm run type-check:watch
-```
+Plugins for MPXL.
---
+++ src/plugins/pixelsort.ts
@@ -0,0 +1,40 @@
+function plugin(p: any, toolData: any) {
+  //const d = p.pixelDensity();
+  p.loadPixels();
+  const toolAreaWidth =
+    toolData.point.x > toolData.endPoint.x
+      ? toolData.point.x - toolData.endPoint.x
+      : toolData.endPoint.x - toolData.point.x;
+  const toolAreaHeight =
+    toolData.point.y > toolData.endPoint.y
+      ? toolData.point.y - toolData.endPoint.y
+      : toolData.endPoint.y - toolData.point.y;
+
+  //let pixels = [];
+
+  for (let i = 0; i < toolAreaHeight; i++) {
+    let rowArray = [];
+    for (let j = 0; j < toolAreaWidth; j++) {
+      let pixelArray = p.get(toolData.point.x + j, toolData.point.y + i);
+      rowArray.push(pixelArray);
+    }
+
+    rowArray.sort((a, b) => {
+      return a[0] - b[0];
+    });
+
+    for (let j = 0; j < toolAreaWidth; j++) {
+      p.set(
+        toolData.point.x + j,
+        toolData.point.y + i,
+        p.color(
+          `rgba(${rowArray[j][0]},${rowArray[j][1]},${rowArray[j][2]},${
+            rowArray[j][3]
+          })`
+        )
+      );
+    }
+  }
+
+  p.updatePixels();
+}
